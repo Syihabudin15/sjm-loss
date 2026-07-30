18c30a04eee041684647326a179b1231929d65a5
Syihabudin Tsani <syihabudintsani15@gmail.com>
2026-07-30 23:15:52 +0700

    imrpove db connextion limit

--- libs/Prisma.ts
+++ libs/Prisma.ts
@@ -7,7 +7,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 2,
+  connectionLimit: 30,
 });
 const prisma = new PrismaClient({ adapter });
 
